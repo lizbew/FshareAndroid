4d06e1ca3fdbb4a2ff044b4b760dd540030232a1
VikaZhou <my12time@gmail.com>
2016-05-29 18:56:17 +0800

    init, add .SEND intent-filter

---
+++ app/src/main/java/com/viifly/fshareandroid/MainActivity.java
@@ -0,0 +1,44 @@
+package com.viifly.fshareandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = (TextView)findViewById(R.id.textView2);
+
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                handleSendImage(intent); // Handle single image being sent
+            }
+        }
+    }
+
+    void handleSendImage(Intent intent) {
+        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            // Update UI to reflect image being shared
+            Log.d(TAG, "Received file share " + imageUri.toString());
+            tv1.setText(imageUri.toString());
+        }
+    }
+}
